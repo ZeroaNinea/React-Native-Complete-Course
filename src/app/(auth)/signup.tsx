37e7a887e18bd831c7f2d0cb19427b6575274367
Catgirl <zeroaninea@gmail.com>
2026-03-31 21:55:27 +0400

    the sign up page updated

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,11 +1,59 @@
 import { ThemedText } from '@/components/themed-text';
-import { StyleSheet, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Signup() {
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <ThemedText type="title">Signup</ThemedText>
-    </View>
+    <SafeAreaView edges={['top', 'bottom']}>
+      <View style={styles.container}>
+        <ThemedText type="title" style={styles.title}>
+          Create Account
+        </ThemedText>
+        <Text style={styles.subtitle}>Sign Up to Get Started</Text>
+        <View style={styles.separator}></View>
+        <View>
+          <TextInput
+            placeholder="Email..."
+            placeholderTextColor={'#999'}
+            keyboardType="email-address"
+            autoComplete="email"
+            autoCapitalize="none"
+            style={styles.textInput}
+          />
+          <TextInput
+            placeholder="Password..."
+            placeholderTextColor={'#999'}
+            secureTextEntry
+            autoComplete="password"
+            autoCapitalize="none"
+            style={styles.textInput}
+          />
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.linkButton}
+            onPress={() => router.push('/signup')}
+          >
+            <Text style={styles.linkButtonText}>
+              Already have an account?{' '}
+              <Text style={styles.linkButtonTextBold}>Sign In</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,5 +62,46 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    marginTop: 20,
+  },
+  subtitle: {
+    color: '#999',
+    fontSize: 20,
+    marginTop: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  textInput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#21bde4',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  linkButton: {
+    marginTop: 20,
+  },
+  linkButtonText: {
+    color: '#666',
+  },
+  linkButtonTextBold: {
+    color: '#666',
+    fontWeight: 'bold',
   },
 });
