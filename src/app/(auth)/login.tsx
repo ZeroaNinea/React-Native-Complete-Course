45e13b6851f0581fc6cf2abafd596b75463225c6
Catgirl <zeroaninea@gmail.com>
2026-03-31 19:32:50 +0400

    the login page styled

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,5 +1,5 @@
 import { ThemedText } from '@/components/themed-text';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
@@ -7,19 +7,24 @@ export default function LoginScreen() {
     <SafeAreaView edges={['top', 'bottom']}>
       <View style={styles.container}>
         <ThemedText type="title">Welcome Back</ThemedText>
-        <ThemedText type="subtitle">Sign In to Continue</ThemedText>
+        <Text style={styles.subtitle}>Sign In to Continue</Text>
+        <View style={styles.separator}></View>
         <View>
           <TextInput
             placeholder="Email..."
             placeholderTextColor={'#999'}
             keyboardType="email-address"
             autoComplete="email"
+            autoCapitalize="none"
+            style={styles.textInput}
           />
           <TextInput
             placeholder="Password..."
             placeholderTextColor={'#999'}
             secureTextEntry
             autoComplete="password"
+            autoCapitalize="none"
+            style={styles.textInput}
           />
         </View>
       </View>
@@ -32,5 +37,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  subtitle: {
+    color: '#999',
+    fontSize: 20,
+    marginTop: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  textInput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
   },
 });
