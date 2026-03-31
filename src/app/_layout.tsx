9deb036b26821c3db4fb4d4a8edc20f8bc62454d
Catgirl <zeroaninea@gmail.com>
2026-03-31 18:01:12 +0400

    checking the auth

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,16 +3,31 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 
 // import { AnimatedSplashOverlay } from '@/components/animated-icon';
 // import AppTabs from '@/components/app-tabs';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
+
+  let isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace('/(auth)/login');
+    } else {
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
+      </Stack>;
+    }
+  });
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       {/* <AnimatedSplashOverlay /> */}
