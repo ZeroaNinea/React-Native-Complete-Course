935d17566514591383df8747730bc0e5d28868a7
Catgirl <zeroaninea@gmail.com>
2026-04-01 14:40:38 +0400

    a subtitle added

--- src/app/(auth)/onboarding.tsx
+++ src/app/(auth)/onboarding.tsx
@@ -1,18 +1,27 @@
 import { ThemedText } from '@/components/themed-text';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Onboarding() {
   return (
-    <View style={style.container}>
+    <View style={styles.container}>
       <ThemedText type="title">Onboarding</ThemedText>
+      <Text style={styles.subtitle}>Sign Up to Get Started</Text>
     </View>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    marginTop: 20,
+  },
+  subtitle: {
+    color: '#999',
+    fontSize: 20,
+    marginTop: 10,
   },
 });
